f016b9d0e5e6f138cc136fcf42b81b634c3082fd
Omar El-Begawy <omar.elbegawy@gmail.com>
2018-11-08 22:50:54 +0200

    Updates

---
+++ precache-manifest.6ba5ef22e5a1a675698bf41f9127b16d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6931197e8eca943ccf4f5e72087ead29",
+    "url": "/web-accessibility/static/media/Hero.6931197e.gif"
+  },
+  {
+    "revision": "473e52c39030f583fd26",
+    "url": "/web-accessibility/static/js/runtime~main.473e52c3.js"
+  },
+  {
+    "revision": "97668fcf84ebd91f514b",
+    "url": "/web-accessibility/static/js/main.97668fcf.chunk.js"
+  },
+  {
+    "revision": "c6bf0b0026bc28eeaebc",
+    "url": "/web-accessibility/static/js/1.c6bf0b00.chunk.js"
+  },
+  {
+    "revision": "97668fcf84ebd91f514b",
+    "url": "/web-accessibility/static/css/main.245a20ed.chunk.css"
+  },
+  {
+    "revision": "9a0d63e2fbf3a99f127e6dedf8e048f7",
+    "url": "/web-accessibility/index.html"
+  }
+];
